e09c58f3b05ef22635805beeec1ebdbde238c05f
gedinakova <gedinakova@infragistics.com>
2019-03-29 15:22:19 +0200

    Adding pwa app.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,9 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { PeopleListComponent } from './people/people-list/people-list.component';
 import { InitialsPipe } from './pipes/initials.pipe';
+import { RouterModule } from '@angular/router';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -20,7 +23,7 @@ import { InitialsPipe } from './pipes/initials.pipe';
 ],
 imports: [
     FormsModule,
-    BrowserModule,
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
     BrowserAnimationsModule,
     AppRoutingModule,
     IgxNavigationDrawerModule,
@@ -28,7 +31,9 @@ imports: [
     IgxLayoutModule,
     IgxRippleModule,
     IgxCardModule,
-    IgxAvatarModule
+    IgxAvatarModule,
+    RouterModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
